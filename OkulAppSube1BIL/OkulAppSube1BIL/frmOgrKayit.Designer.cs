2f524e0840bb6912bdf8ef95bba019c424b4a588
Gazi <Gazi@DESKTOP-224SE1B>
2023-12-12 10:14:11 +0300

    Insert Islemleri

---
+++ OkulAppSube1BIL/OkulAppSube1BIL/frmOgrKayit.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace OkulAppSube1BIL
+{
+    partial class frmOgrKayit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpOgrenci = new System.Windows.Forms.GroupBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblNumara = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.grpOgrenci.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpOgrenci
+            // 
+            this.grpOgrenci.Controls.Add(this.lblNumara);
+            this.grpOgrenci.Controls.Add(this.lblSoyad);
+            this.grpOgrenci.Controls.Add(this.lblAd);
+            this.grpOgrenci.Controls.Add(this.txtNumara);
+            this.grpOgrenci.Controls.Add(this.txtSoyad);
+            this.grpOgrenci.Controls.Add(this.txtAd);
+            this.grpOgrenci.Location = new System.Drawing.Point(12, 12);
+            this.grpOgrenci.Name = "grpOgrenci";
+            this.grpOgrenci.Size = new System.Drawing.Size(339, 202);
+            this.grpOgrenci.TabIndex = 0;
+            this.grpOgrenci.TabStop = false;
+            this.grpOgrenci.Text = "Öğrenci Bilgileri";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(122, 72);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 20);
+            this.txtAd.TabIndex = 0;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(122, 100);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtSoyad.TabIndex = 1;
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Location = new System.Drawing.Point(122, 129);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(100, 20);
+            this.txtNumara.TabIndex = 2;
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(70, 79);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(20, 13);
+            this.lblAd.TabIndex = 3;
+            this.lblAd.Text = "Ad";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(70, 107);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(37, 13);
+            this.lblSoyad.TabIndex = 4;
+            this.lblSoyad.Text = "Soyad";
+            // 
+            // lblNumara
+            // 
+            this.lblNumara.AutoSize = true;
+            this.lblNumara.Location = new System.Drawing.Point(70, 132);
+            this.lblNumara.Name = "lblNumara";
+            this.lblNumara.Size = new System.Drawing.Size(44, 13);
+            this.lblNumara.TabIndex = 5;
+            this.lblNumara.Text = "Numara";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(134, 227);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(100, 23);
+            this.btnKaydet.TabIndex = 6;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // frmOgrKayit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 269);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.grpOgrenci);
+            this.Name = "frmOgrKayit";
+            this.Text = "Öğrenci Kayıt İşlemleri";
+            this.grpOgrenci.ResumeLayout(false);
+            this.grpOgrenci.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpOgrenci;
+        private System.Windows.Forms.Label lblNumara;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.TextBox txtNumara;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnKaydet;
+    }
+}
+
---
+++ OkulAppSube1BIL/OkulAppSube1BIL/frmOgrKayit.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OkulAppSube1BIL
+{
+    public partial class frmOgrKayit : Form
+    {
+        public frmOgrKayit()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = null;
+            try
+            {
+                cn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=OkulDbSube1BIL;Integrated Security=true");
+                SqlCommand cmd = new SqlCommand($"Insert into tblOgrenciler values ('{txtAd.Text.Trim()}','{txtSoyad.Text.Trim()}','{txtNumara.Text.Trim()}')", cn);
+                cn.Open();
+                int sonuc = cmd.ExecuteNonQuery();
+                MessageBox.Show(sonuc > 0 ? "Ekleme Başarılı" : "Ekleme Başarısız!");
+            }
+            catch (SqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 2627:
+                        MessageBox.Show("Bu numaralı öğrenci daha önce kayıtlı");
+                        break;
+                    default:
+                        MessageBox.Show("Veritabanı hatası");
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bir hata oluştu!");
+            }
+            finally
+            {
+                if (cn != null && cn.State != ConnectionState.Closed)
+                {
+                    cn.Close();
+                }
+            }
+        }
+    }
+}
