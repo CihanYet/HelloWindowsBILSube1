edf7617122d9d4afe18179e0c93b6e1441241401
Gazi <Gazi@DESKTOP-224SE1B>
2023-12-19 10:06:00 +0300

    -

---
+++ OkulAppSube1BIL/OkulApp.BLL/OgrenciBL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace OkulApp.BLL
+{
+    public class OgrenciBL
+    {
+        public bool OgrenciEkle(Ogrenci ogr)
+        {
+            SqlConnection cn = null;
+            SqlCommand cmd = null;
+            try
+            {
+                using (cn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=OkulDbSube1BIL;Integrated Security=true"))
+                {
+                    using (cmd = new SqlCommand($"Insert into tblOgrenciler values (@Ad,@Soyad,@Numara)", cn))
+                    {
+                        SqlParameter[] p = {
+                             new SqlParameter("@Ad",ogr.Ad),
+                             new SqlParameter("@Soyad",ogr.Soyad),
+                             new SqlParameter("@Numara",ogr.Numara)
+                         };
+                        cmd.Parameters.AddRange(p);
+
+                        cn.Open();
+                        int sonuc = cmd.ExecuteNonQuery();
+                        return sonuc > 0;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            //finally
+            //{
+            //    if (cn != null && cn.State != ConnectionState.Closed)
+            //    {
+            //        cn.Close();
+            //        cn.Dispose();
+            //        cmd.Dispose();
+            //    }
+            //}
+        }
+    }
+}
+//n Katmanlı Mimari
--- OkulAppSube1BIL/OkulAppSube1BIL/frmOgrKayit.cs
+++ OkulAppSube1BIL/OkulAppSube1BIL/frmOgrKayit.cs
@@ -19,48 +19,7 @@ namespace OkulAppSube1BIL
             InitializeComponent();
         }
 
-        bool OgrenciEkle(Ogrenci ogr)
-        {
-            SqlConnection cn = null;
-            SqlCommand cmd = null;
-            try
-            {
-                using (cn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=OkulDbSube1BIL;Integrated Security=true"))
-                {
-                    using (cmd = new SqlCommand($"Insert into tblOgrenciler values (@Ad,@Soyad,@Numara)", cn))
-                    {
-                        SqlParameter[] p = {
-                             new SqlParameter("@Ad",ogr.Ad),
-                             new SqlParameter("@Soyad",ogr.Soyad),
-                             new SqlParameter("@Numara",ogr.Numara)
-                         };
-
-                        cmd.Parameters.AddRange(p);
-
-                        cn.Open();
-                        int sonuc = cmd.ExecuteNonQuery();
-                        return sonuc > 0;
-                    }
-                }
-            }
-            catch (SqlException ex)
-            {
-                throw ex;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-            //finally
-            //{
-            //    if (cn != null && cn.State != ConnectionState.Closed)
-            //    {
-            //        cn.Close();
-            //        cn.Dispose();
-            //        cmd.Dispose();
-            //    }
-            //}
-        }
+       
 
         private void btnKaydet_Click(object sender, EventArgs e)
         {
